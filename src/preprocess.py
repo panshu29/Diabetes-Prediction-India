b64281606eab2ec8703ef1d9d664799b9fc4580c
anshubana <anshupmail@gmail.com>
2025-08-20 15:35:40 +0530

    ok

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,23 +10,25 @@ def preprocess(inputPath,outputPath):
     data=pd.read_csv(inputPath)
     os.makedirs(os.path.dirname(outputPath),exist_ok=True)
     
-    #All preprocessing logic here (if any)...
+    #All following preprocessing logic goes here...
     print("...Data Cleaning (step2)... ")
     print("...Data Transformation (step3)... ")
     print("...Data Integration(step4)... ")
     print("...Data Reduction (step5)... ")
     print(".. Data Splitting (step6)... ")
-    print(".. Data Preprocessing is done... ")
-
+    
     #Finally write the processed file
     data.to_csv(outputPath,header=None,index=False)
     print(f"Processed File is in {outputPath}")
 
 
+
 if __name__ == '__main__':
+    print(f"** Data preprocessing started ! **")
     params=yaml.safe_load(open('../params.yaml'))
     inputPath= "../"+params['preprocess']['input']
     outputPath= "../"+params['preprocess']['output']
     preprocess(inputPath,outputPath)
+    print("** Data Preprocessing is done ** ")
 
 
